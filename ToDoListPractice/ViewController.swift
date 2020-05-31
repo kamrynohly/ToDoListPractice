622acfb97f5fbe188f03de326cc258ef470cc66f
Kamryn Ohly <kamohly@gmail.com>
2020-05-31 07:00:21 -0400

    Initial commit

---
+++ ToDoListPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListPractice
+//
+//  Created by Kamryn Ohly on 5/31/20.
+//  Copyright © 2020 Kamryn Ohly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
